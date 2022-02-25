51b7aba94b70623134c9acbff019d1126e5754fe
FyreByrd <76973631+FyreByrd@users.noreply.github.com>
2022-02-25 15:34:08 -0500

    New Board class
    -class Ship
    -class Board
    doing a complete rewrite of the program to make it run better and look more professional

--- board.py
+++ board.py
@@ -1,133 +1,190 @@
-class Board:
-    def __init__(self):
-        self.board = []
-        self.rows = {"a":0,"b":1,"c":2,"d":3,"e":4,"f":5,"g":6,"h":7,"i":8,"j":9}
-        self.raw = []
-        i = 0
-        for i in range(10):
-            row = " . . . . . . . . . "
-            self.board.append(row)
-        for i in range(100):
-            self.raw.append("0")
-    def display_board(self):
-        letters = ('A','B','C','D','E','F','G','H','I','J')
-        output = ["    1.2.3.4.5.6.7.8.9.10"]
-        for i in range(len(self.board)):
-            output.append(" "+letters[i]+"| "+self.board[i]+" |")
-        return "\n".join(output)
-    def coords_from_input(self, row, col):
-        col = int(col)
-        if col == 0:
-            col = 10
-        return (self.rows[row], 2*(col-1))
-    def rawcoord_from_input(self, row, col):
-        col  = int(col)
-        if col == 0:
-            col = 10
-        row = self.rows[row]
-        return (10*row)+(col-1)
-    def coords_from_rawcoord(self, rcoord):
-        rcoord = int(rcoord)
-        return ((rcoord - (rcoord % 10)) // 10, (rcoord % 10) * 2)
-    def rawcoord_from_coords(self, coords):
-        return (coords[0] * 10) + (coords[1] // 2)
-    def north(self, coord):
-        return "" if coord[0] <= 0 else (coord[0] - 1, coord[1])
-    def east(self, coord):
-        return "" if coord[1] >= 9 else (coord[0], coord[1] + 2)
-    def south(self, coord):
-        return "" if coord[0] >= 9 else (coord[0] + 1, coord[1])
-    def west(self, coord):
-        return "" if coord[1] <= 0 else (coord[0], coord[1] - 2)
-    def neighbors_of_coord(self, coords):
-        n = self.north(coords)
-        e = self.east(coords)
-        s = self.south(coords)
-        w = self.west(coords)
-        neighbors = []
-        if not n == "":
-            neighbors.append(n)
-        if not e == "":
-            neighbors.append(e)
-        if not s == "":
-            neighbors.append(s)
-        if not w == "":
-            neighbors.append(w)
-        return neighbors
-    def val_at_coords(self, coords):
-        try:
-            s = self.board[coords[0]]
-            return s[coords[1]]
-        except IndexError:
-            print("IndexError")
-            print(self.display_board())
-            print(str(coords)+" "+str(s)+" "+str(len(s)))
-            raise
-    def set_at_coords(self, coords, val):
-        row = self.board[coords[0]]
-        x = coords[1]
-        row = row[:x]+str(val)+row[x+1:]
-        self.board[coords[0]] = row
-    def guess(self, coords, radar):
-        a = coords[0]
-        b = coords[1]
-        test = self.val_at_coords(coords)
-        if test == "X" or test == "-" or radar.val_at_coords(coords) == "O" or radar.val_at_coords(coords) == "X":
-            return " Already guessed. Try again"
-        elif test == " ":
-            radar.set_at_coords(coords, "O")
-            self.set_at_coords(coords, "-")
-            return " Miss!"
-        else:
-            self.set_at_coords(coords, "X")
-            rc = self.rawcoord_from_coords(coords)
-            test = self.raw[rc]
-            self.raw[rc] = "X"
-            radar.set_at_coords(coords, "X")
-            if test not in self.raw:
-                return " Hit and Sunk!"
-            return " Hit!"
-    def try_insert_ship(self, coords, length, dir):
-        if self.val_at_coords(coords) == " ":
-            if dir == 0:
-                ncoords = coords
-                for i in range(length):
-                    ncoords = self.north(ncoords)
-                    if ncoords == "" or not (self.val_at_coords(ncoords) == " "):
-                        return "ship won't fit. try new position or orientation"
-            elif dir == 1:
-                ncoords = coords
-                for i in range(length):
-                    ncoords = self.east(ncoords)
-                    if ncoords == "" or not (self.val_at_coords(ncoords) == " "):
-                        return "ship won't fit. try new position or orientation"
-            else:
-                return "invalid direction"
-            return "ship placed successfully"
-        else:
-            return "coordinates occupied"
+#board.py: contains classes Ship, and Board
+#Author: Aidan Jones
 
-    def insert_ship(self, coords, ship, ship_id, dir):
-        y = coords[0]
-        x = coords[1]
-        test = self.try_insert_ship(coords, len(ship), dir)
-        if "success" not in test:
-            return test
-        if dir == 1:
-            s = self.board[y]
-            insert = s[:x]
-            a = x
-            for c in ship:
-                insert += c+"."
-                self.raw[self.rawcoord_from_coords((y,a))] = str(ship_id)
-                a += 2
-            b = 2 * len(ship)
-            insert += s[x+b:]
-            self.board[y] = insert
+#<<<<<Import Statements>>>>>
+from random import randrange
+
+#--container for text representations of the ships
+class Ship:
+    def __init__(this):
+        horizontal = ["[###]","<ss>","co>","cf>","c>"]
+        vertical = ["=HHH=","^HHV","^OU","^@U","^U"]
+        this.ships = (horizontal, vertical)
+        this.uniq = {"#":0,"=":0,"s":1,"V":1,"o":2,"O":2,"f":3,"@":3}
+
+#--Represents a player's playing area
+class Board:
+    #<<<<<Constructor>>>>>
+    def __init__(this):
+        this.data = {"board":"0"*100, "radar":"0"*100, "raw":"0"*100}
+        this.rows = "abcdefghij"
+        this.shipdict = Ship()
+    
+    #<<<<<Output Functions>>>>>
+    #--formats board as string for printing
+    def __str__(this):
+        out = " _________________________ \n"
+        out +="|## 1 2 3 4 5 6 7 8 9 10##|\n"
+        out +="|#########################|\n"
+        for i in range(10):
+            out += "|"+this.rows[i]+"#"
+            for j in range(10):
+                k = j + 10*i
+                out += "."+str(this.dat2char(k, "radar"))
+            out += ".#"+this.rows[i]+"|\n"
+        out +="|#########################|\n"
+        out +="|## 1 2 3 4 5 6 7 8 9 10##|\n"
+        out +="'^^^^^^^^^^^^^^^^^^^^^^^^^'\n"
+        out +="|VVVVVVVVVVVVVVVVVVVVVVVVV|\n"
+        out +="|## 1 2 3 4 5 6 7 8 9 10##|\n"
+        out +="|#########################|\n"
+        for i in range(10):
+            out += "|"+this.rows[i]+"#"
+            for j in range(10):
+                k = j + 10*i
+                out += "."+str(this.dat2char(k, "board"))
+            out += ".#"+this.rows[i]+"|\n"
+        out +="|#########################|\n"
+        out +="|## 1 2 3 4 5 6 7 8 9 10##|\n"
+        out +="'^^^^^^^^^^^^^^^^^^^^^^^^^'"
+        return out
+    #--inputs guess(g) into the board, then returns:
+    #----"Already guessed." if g has been guessed before
+    #----"Miss." if the guess is a miss
+    #----"Hit and Sunk." if it is a sinking hit
+    #----or "Hit." if it is a non-sinking hit
+    #--will return an error message if the guess is out of bounds 
+    def guess(this, g):
+        g = this.conv2int(g)
+        if g not in range(100):
+            print("Error: guess out of bounds of board")
         else:
-            a = y
-            for c in ship:
-                self.board[a] = self.board[a][:x] + c + self.board[a][x+1:]
-                self.raw[self.rawcoord_from_coords((a,x))] = str(ship_id)
-                a -= 1
-        return test
+            t = this.data["raw"][g]
+        if t == "G":
+            return "Already guessed."
+        elif t == "0":
+            this.insert("G", g, "raw")
+            this.insert("~", g, "board")
+            return "Miss."
+        else:
+            x = this.insert("G", g, "raw")
+            this.insert("!", g, "board")
+            if x not in this.data["raw"]:
+                return "Hit and Sunk."
+            else:
+                return "Hit."
+    #--returns just the radar portion of the board as a string
+    def radar(this):
+        s = this.__str__()
+        return s[:447]
+    
+    #<<<<<Helper Functions>>>>>
+    #--returns a more helpful representation of the data in the board selected by arr
+    def dat2char(this, i:int, arr:str):
+        arr = this.data[arr]
+        if arr[i] == "0":
+            return " "
+        elif arr[i] == "~":
+            return "~"
+        elif arr[i] == "!":
+            return "&"
+        else:
+            return arr[i]
+    #--converts i to an integral index for the arrays
+    #----expected types:
+    #------ int: [0,99]
+    #------ tuple : (str : [a,j] or int: [0,9] , int: [0,9], ...)
+    #------ str : "[a-j][0-9]*"
+    #----will not throw error if int is out of specified range
+    #----will throw error if:
+    #------input is not one of the above types
+    #------the second index cannot be converted to int
+    #------the first index is not in range
+    def conv2int(this, i):
+        if type(i) == int:
+            return i
+        elif type(i) == tuple:
+            if type(i[0]) == str:
+                return 10 * this.rows.index(i[0]) + int(i[1])
+            else:
+                return 10 * int(i[0]) + int(i[1])
+        elif type(i) == str:
+            return 10 * this.rows.index(i[0]) + int(i[1])
+        else:
+            raise ValueError
+    #--inserts c in board arr at index ind and returns the old value at ind
+    def insert(this, c, ind:int, arr:str):
+        ind = this.conv2int(ind)
+        if ind not in range(100):
+            raise ValueError
+        else:
+            t = this.data[arr][ind]
+            this.data[arr] = this.data[arr][:ind]+str(c)+this.data[arr][ind+1:]
+            return t
+    #--clears the board
+    def clear_board(this):
+        this.data = {"board":"0"*100, "radar":"0"*100, "raw":"0"*100}
+    
+    #<<<<<Ship Insertion Functions>>>>>
+    #--generates a random board
+    def randomize(this):
+        ships = [(5,"5"),(4,"4"),(3,"3"),(3,"2"),(2,"1")]
+        for s in range(len(ships)):
+            success = False
+            while not success:
+                dir = randrange(0,2)
+                ind = randrange(0, 100)
+                success = this.insert_ship(ind,dir,s,ships[s][1])         
+    #--inserts a ship at index ind with orientation dir and raw representation ch
+    #----dir is 0 for horizontal and 1 for vertical
+    #----returns False if ship cannot be placed as specified
+    def insert_ship(this, ind:int, dir:int, ship:int, ch:str):
+        s = this.shipdict.ships[dir][ship]
+        l = len(s)
+        available = True
+        for i in range(ind, ind + l * 10 ** dir, 10 ** dir):
+            if i >= 100:
+                available = False
+            elif this.data["raw"][i] != "0":
+                available = False
+            elif i % 10 == 9:
+                if i < 99:
+                    j = i + 10**dir
+                    if j % 10 == 0:
+                        available = False 
+        if available:
+            n = range(l)
+            inds = range(ind, ind + l * 10 ** dir, 10 ** dir)
+            for i in n:
+                this.insert(s[i],inds[i],"board")
+                this.insert(ch,inds[i],"raw")
+        return available
+    #--removes a ship matching ch in the raw data array
+    def remove_ship(this, ch:str):
+        s = ""
+        while ch in this.data["raw"]:
+            i = this.data["raw"].index(ch)
+            this.insert("0", i, "raw")
+            s += this.insert("0", i, "board")
+        r = this.find_ship_by_uniq(s)
+        if r[0]:
+            return "Removed: "+r[1]
+        else:
+            return "No ship was removed."
+    #--searches for horizontal representation of ship by indicators in s
+    def find_ship_by_uniq(this, s:str):
+        match = False
+        for c in s:
+            try:
+                if not match:
+                    s = this.shipdict.ships[0][this.shipdict.uniq[c]]
+                    match = True
+            except ValueError:
+                match = False
+                continue
+        if not match:
+            if len(s) == 2:
+                s = this.shipdict.ships[0][4]
+                match = True
+        return (match, s)
+    
