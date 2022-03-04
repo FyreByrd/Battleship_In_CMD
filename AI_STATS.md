071c033f2739500afce04934b208a5fcb15bed23
Aidan Jones <ogonivanovich@gmail.com>
2022-03-03 21:23:05 -0500

    Added BasicAI!
    -Added BasicAI
    -Crunched numbers for BasicAI and StupidAI at 1 million iterations

--- AI_STATS.md
+++ AI_STATS.md
@@ -1,18 +1,29 @@
 # Statistics for the AI
+Tested on an HP laptop with intel CORE i7 8th Gen and 8GB RAM
 ## Solo
 A test of how the AI performs on randomly generated boards.
 ### StupidAI
-Iterations: 100,000  
-Time:       1:19.93
+Iterations: 1,000,000  
+Time:       14:09.79  
+Avg. Guess: 8.909 &mu;s
 |             | Mean  | Range    |
 |---          |:-:    |:-:       |
-| First Sunk  | 50.53 | [2, 94]  |
-| Second Sunk | 68.91 | [13, 97] |
-| Third Sunk  | 80.24 | [28, 98] |
-| Fourth Sunk | 88.67 | [39, 99] |
-| Gameover    | 95.39 | [55, 100]|
+| First Sunk  | 50.55 | [2, 96]  |
+| Second Sunk | 68.88 | [8, 97]  |
+| Third Sunk  | 80.27 | [21, 98] |
+| Fourth Sunk | 88.68 | [31, 99] |
+| Gameover    | 95.39 | [44, 100]|
 ### BasicAI
-Untested
+Iterations: 1,000,000  
+Time:       8:31.64
+Avg. Guess: 8.230 &mu;s
+|             | Mean  | Range    |
+|---          |:-:    |:-:       |
+| First Sunk  | 12.06 | [2, 51]  |
+| Second Sunk | 23.70 | [5, 65]  |
+| Third Sunk  | 35.49 | [8, 72]  |
+| Fourth Sunk | 47.95 | [14, 79] |
+| Gameover    | 62.17 | [23, 100]|
 ### AdvancedAI
 Untested
 
--- player.py
+++ player.py
@@ -54,12 +54,11 @@ class AIPlayer(Player):
 class StupidAI(AIPlayer):
     def __init__(this, name:str="AI (Easy)"):
         super().__init__(name)
-        this.sunkcount = 0
         this.guesses = []
     def turn(this, opp:Player):
         success = False
-        if len(this.guesses) >= 100 or this.sunkcount >= 5:
-            return "Gameover."
+        if len(this.guesses) >= 100 or this.score >= 5:
+            return ("Gameover.","")
         while not success:
             x = rand(0, 100)
             if x in this.guesses:
@@ -68,13 +67,73 @@ class StupidAI(AIPlayer):
                 t = opp.get_board().guess(x)
                 this.guesses.append(x)
                 if "Sunk" in t[0]:
-                    this.sunkcount += 1
+                    this.score += 1
                 return t
-
-#VVV#NOT IMPLEMENTED#VVV#
 #--AI that uses a basic 2-stage Hunt/Target algorithm
 class BasicAI(AIPlayer):
-    pass
+    #--constructor
+    def __init__(this, name:str="AI (Normal)"):
+        super().__init__(name)
+        this.parity = []
+        this.all = []
+        #builds list of valid guesses
+        for i in range(100):
+            this.all.append(i)
+        #builds list of parity guesses
+        flip = 0
+        for i in range(1, 100, 2):
+            this.parity.append(i-flip)
+            if i % 10 == 9:
+                flip = 1 if flip == 0 else 0
+        this.to_try = [] #coordinates to try
+        this.mode = "Hunt"
+    #--returns a random coordinate from parity or all
+    def get_random_coord(this):
+        if len(this.parity) > 0:
+            r = rand(0,len(this.parity))
+            i = this.parity[r]
+            this.parity.remove(i)
+            this.all.remove(i)
+            return i
+        elif len(this.all) > 0:
+            r = rand(0,len(this.all))
+            i = this.all[r]
+            this.all.remove(i)
+            return i
+        else:
+            print("Out of coordinates to guess")
+            raise IndexError
+    #--2-stage HUNT/TARGET algorithm with parity
+    def turn(this, opp:Player):
+        if this.score >= 5:
+            return ("Gameover.", "")
+        t = (" "," ")
+        if len(this.to_try) > 0:
+            this.mode = "TARGET"
+        #TARGET mode
+        if this.mode == "TARGET" and len(this.to_try) > 0:
+            i = this.to_try.pop()
+            this.all.remove(i)
+            try:
+                this.parity.remove(i)
+            except ValueError:
+                pass
+            t = opp.get_board().guess(i)
+        #HUNT mode
+        else:
+            i = this.get_random_coord()
+            t = opp.get_board().guess(i)
+        if "Hit" in t[0]:
+            this.mode = "TARGET"
+            neighbors = this.board.neighbors(i)
+            for n in neighbors:
+                if (n not in this.to_try) and (n in this.all) and (n != -1):
+                    this.to_try.append(n)
+            if "Sunk" in t[0]:
+                this.score += 1
+        return t
+
+#VVV#NOT IMPLEMENTED#VVV#
 #--AI that uses a Probability Density Function to aid a 2-stage Hunt/Target algorithm
 class AdvancedAI(AIPlayer):
     pass
